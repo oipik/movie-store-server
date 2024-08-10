0c5a3ebe151b35f7f32bb3d6f455f1e23f130cb6
oipik <wazuup.lw@mail.ru>
2024-08-10 09:24:39 +0300

    fix

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = config.get('port') ?? 3003;
 
 mongoose.connect(
-  config.get('mongoUri')
+  process.env.MONGODB_URI || 'mongodb+srv://admin:admin@cluster1.jflpjkq.mongodb.net/db?retryWrites=true&w=majority&appName=Cluster1'
 ).then(() => {
   console.log('DB connected!');
 }).catch(err => {
@@ -25,7 +25,7 @@ app.post('/auth/login', loginValidation, handleValidationErrors, UserController.
 app.post('/auth/register', registerValidation, handleValidationErrors, UserController.register)
 app.get('/auth/me', checkAuth, UserController.getMe)
 
-app.listen(PORT, (err) => {
+app.listen(process.env.PORT || PORT, (err) => {
   if (err) {
     return console.log(err);
   }
